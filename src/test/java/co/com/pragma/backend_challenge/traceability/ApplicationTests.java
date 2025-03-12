a1778f33603a3f1f705d5d4d2e26d64465201f1c
Gerardo Castillo <gerardo.castillo@pragma.com.co>
2025-03-12 16:46:53 -0500

    initial commit

---
+++ src/test/java/co/com/pragma/backend_challenge/traceability/ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.pragma.backend_challenge.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
